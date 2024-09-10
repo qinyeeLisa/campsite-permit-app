4a48f4f85401b771bbe11aa91f3e5fad077e1880
QIN YEE LIM <limqinyeelisa@gmail.com>
2024-09-10 23:13:13 +0800

    Fix error

--- lib/Screens/login_signup.dart
+++ lib/Screens/login_signup.dart
@@ -1,7 +1,7 @@
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:camplified/amplifyconfiguration.dart';
+import 'package:camplified/amplify_configuration.dart';
 import 'package:flutter/material.dart';
 
 void main() {
