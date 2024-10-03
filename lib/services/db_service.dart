66fc6e016fc30ff97c0258a1e04a5ca7fe264aaf
qamarularifin <qamarulafro89@gmail.com>
2024-10-04 01:20:01 +0800

    getpermitbyid

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -3,20 +3,21 @@ import 'dart:developer';
 import 'package:camplified/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseService{
+class DatabaseService {
   final _firebase = FirebaseFirestore.instance;
 
   Future<int> getCurrentIdCount() async {
-    final QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('users').get();
+    final QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('users').get();
     return querySnapshot.docs.length;
-}
+  }
 
-  createUser(UserModel user) async{
-    try{
+  createUser(UserModel user) async {
+    try {
       int currentCount = await getCurrentIdCount();
 
       _firebase.collection('users').add({
-        'id': currentCount + 1,
+        'userId': currentCount + 1,
         'fullName': user.fullName,
         'email': user.email,
         //'password': user.password,
@@ -26,35 +27,37 @@ class DatabaseService{
       });
 
       return true;
-    }catch(e){
+    } catch (e) {
       log(e.toString());
     }
   }
 
-  retrieveUsers(String email) async{
-    try{
-      final userDoc = await _firebase.collection('users')
-        .where('email', isEqualTo: email)
-        .get();
+  retrieveUsers(String email) async {
+    try {
+      final userDoc = await _firebase
+          .collection('users')
+          .where('email', isEqualTo: email)
+          .get();
 
       return userDoc.docs.first.data() as Map<String, dynamic>;
-    }catch(e){
+    } catch (e) {
       log(e.toString());
     }
   }
 
-  Future<bool> checkUserExist(String email) async{
-    try{
-      final userDoc = await _firebase.collection('users')
-        .where('email', isEqualTo: email)
-        .get();
+  Future<bool> checkUserExist(String email) async {
+    try {
+      final userDoc = await _firebase
+          .collection('users')
+          .where('email', isEqualTo: email)
+          .get();
 
-        if (userDoc.size > 0) {
-          return true;
-        } else {
-          return false;
-        }
-      } catch(e){
+      if (userDoc.size > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
       log(e.toString());
       return false;
     }
